2f7f3743a6906e65949b4264084d4e79086d454b
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-03 14:47:48 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * generate <https://github.com/jonschlinkert/generate>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
