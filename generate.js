76c23b1d2fb99dbc209c2468d6c61eee3c6d9ddb
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-03 03:59:59 -0400

    clean up

--- generate.js
+++ generate.js
@@ -1,23 +1,8 @@
 
-var relative = require('relative');
 var generate = require('./');
-var del = require('del');
 
 generate.task('default', function () {
-  console.log('starting...');
-  generate.src('*.*')
-    .pipe(generate.dest('./actual'))
-    // .on('end', function (cb) {
-    //   process.nextTick(function () {
-    //     del('actual', cb);
-    //   })
-    // });
-    .on('data', function (file) {
-      console.log('writing...', relative(file.path));
-    })
-    .on('end', function () {
-      console.log('done.');
-    });
+  generate.src('*.*').pipe(generate.dest('./actual'))
 });
 
 generate.run();
