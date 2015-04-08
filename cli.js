37763df32e274f9243857b4b3c1a44e718ac1bf3
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-07 21:47:41 -0400

    start adding logic to cli for handling tasks

--- cli.js
+++ cli.js
@@ -2,38 +2,85 @@
 
 var path = require('path');
 var argv = require('minimist')(process.argv.slice(2));
-var app = require('./');
+var taskTree = require('./lib/utils/task-tree');
+var generate = require('./');
 
-var stack = [];
+
+var stack = argv._;
+var tasks = stack.length ? stack : ['default'];
+
 argv._.forEach(function (name) {
-  stack.push(namify(name));
-  var match = app.generator(namify(name));
-  if (match) {
-    var cwd = path.dirname(match);
-    app.set('cwd', cwd);
-    app.set('templates', cwd + '/templates');
-    app.emit('loaded');
-    require(match);
+  var generator = generate.generator(name);
+  if (generator.module) {
+    var cwd = path.dirname(generator.module);
+    generate.set('cwd', cwd);
+    generate.set('templates', cwd + '/templates');
+    generate.emit('loaded');
+
+    var instance = require(generator.module);
+    process.nextTick(function () {
+      instance.run.apply(instance, tasks);
+    });
   }
 });
 
+generate.on('last', function () {
+  if (argv.set) {
+    var args = argv.set.split('=');
+    generate.store.set.generately(generate.store, args);
+  }
 
-if (argv.set) {
-  var args = argv.set.split('=');
-  app.store.set.apply(app.store, args);
-}
+  if (argv.has) {
+    var args = argv.has.split('=');
+    generate.store.has.generately(generate.store, args);
+  }
 
-if (argv.has) {
-  var args = argv.has.split('=');
-  app.store.has.apply(app.store, args);
-}
+  if (argv.omit) {
+    var args = argv.omit.split('=');
+    generate.store.omit.generately(generate.store, args);
+  }
+  console.log(generate)
+});
 
-if (argv.omit) {
-  var args = argv.omit.split('=');
-  app.store.omit.apply(app.store, args);
-}
+// generate.on('err', function () {
+//   failed = true;
+// });
+
+// generate.on('task_start', function (e) {
+//   console.log('starting', '\'' + chalk.cyan(e.task) + '\'');
+// });
+
+// generate.on('task_stop', function (e) {
+//   var time = prettyTime(e.hrDuration);
+//   console.log('finished', '\'' + chalk.cyan(e.task) + '\'', 'after', chalk.magenta(e.hrDuration));
+// });
+
+// generate.on('task_err', function (e) {
+//   console.log(chalk.cyan(e.task), chalk.red('errored after'), chalk.magenta(e.hrDuration));
+// });
+
+// generate.on('task_not_found', function (err) {
+//   console.log(chalk.red('task \'' + err.task + '\' is not in your generate.js'));
+//   console.log('please check the documentation for proper generate.js formatting');
+//   exit(1);
+// });
+
+// function logTasks(env, instance) {
+//   var tree = taskTree(instance.tasks);
+//   tree.label = 'Tasks for ' + tildify(instance.module);
+//   archy(tree).split('\n').forEach(function (v) {
+//     if (v.trim().length === 0) {
+//       return;
+//     }
+//     gutil.log(v);
+//   });
+// }
 
 // placeholder
 function namify(name) {
   return 'generate-' + name;
+}
+
+if (!argv._.length) {
+  generate.emit('loaded');
 }
