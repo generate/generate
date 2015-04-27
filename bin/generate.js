f00bdb706136f46f85bb04d35c719bdd6d70547e
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-27 04:42:15 -0400

    move cli

--- bin/generate.js
+++ bin/generate.js
@@ -3,9 +3,12 @@
 var path = require('path');
 var chalk = require('chalk');
 var prettyTime = require('pretty-hrtime');
+var completion = require('../lib/utils/completion');
+var taskTree = require('../lib/utils/task-tree');
+var generate = require('..');
+
 var argv = require('minimist')(process.argv.slice(2));
-var taskTree = require('./lib/utils/task-tree');
-var generate = require('./');
+generate.extend('args', argv);
 
 var stack = argv._;
 var name = stack.shift();
@@ -18,13 +21,13 @@ if (file) {
   var cwd = path.dirname(file);
   generate.set('generator.cwd', cwd);
   generate.set('generator.templates', cwd + '/templates');
-  generate.set('args', argv);
   generate.emit('loaded');
 
   var instance = require(file);
+
   process.nextTick(function () {
-    instance.run.apply(instance, tasks);
-  });
+    instance.start.apply(instance, tasks);
+  }.bind(instance));
 }
 
 // exit with 0 or 1
@@ -58,43 +61,42 @@ generate.on('last', function () {
 });
 
 
-// generate.on('err', function () {
-//   failed = true;
-// });
+generate.on('err', function () {
+  failed = true;
+});
 
-// generate.on('task_start', function (e) {
-//   console.log('starting', '\'' + chalk.cyan(e.task) + '\'');
-// });
+generate.on('task_start', function (e) {
+  console.log('starting', '\'' + chalk.cyan(e.task) + '\'');
+});
 
-// generate.on('task_stop', function (e) {
-//   var time = prettyTime(e.hrDuration);
-//   console.log('finished', '\'' + chalk.cyan(e.task) + '\'', 'after', chalk.magenta(time));
-// });
+generate.on('task_stop', function (e) {
+  var time = prettyTime(e.hrDuration);
+  console.log('finished', '\'' + chalk.cyan(e.task) + '\'', 'after', chalk.magenta(time));
+});
 
-// generate.on('task_err', function (e) {
-//   var msg = formatError(e);
-//   var time = prettyTime(e.hrDuration);
-//   console.log(chalk.cyan(e.task), chalk.red('errored after'), chalk.magenta(time));
-//   console.log(msg);
-// });
+generate.on('task_err', function (e) {
+  var msg = formatError(e);
+  var time = prettyTime(e.hrDuration);
+  console.log(chalk.cyan(e.task), chalk.red('errored after'), chalk.magenta(time));
+  console.log(msg);
+});
 
-// generate.on('task_not_found', function (err) {
-//   console.log(chalk.red('task \'' + err.task + '\' is not in your verbfile'));
-//   console.log('please check the documentation for proper verbfile formatting');
-//   exit(1);
-// });
+generate.on('task_not_found', function (err) {
+  console.log(chalk.red('task \'' + err.task + '\' is not in your verbfile'));
+  console.log('please check the documentation for proper verbfile formatting');
+  exit(1);
+});
 
-// function logTasks(env, instance) {
-//   var tree = taskTree(instance.tasks);
-//   tree.label = 'Tasks for ' + tildify(instance.module);
-//   archy(tree).split('\n').forEach(function (v) {
-//     if (v.trim().length === 0) {
-//       return;
-//     }
-//     console.log(v);
-//   });
-// }
-
+function logTasks(env, instance) {
+  var tree = taskTree(instance.tasks);
+  tree.label = 'Tasks for ' + tildify(instance.module);
+  archy(tree).split('\n').forEach(function (v) {
+    if (v.trim().length === 0) {
+      return;
+    }
+    console.log(v);
+  });
+}
 
 // format orchestrator errors
 function formatError(e) {
