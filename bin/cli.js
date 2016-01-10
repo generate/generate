ed3236feb85ceff8d4bc3c7eb1407cb13f956a4b
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-10 18:55:07 -0500

    minor refactor of bin/cli.js
    this still needs refactoring. I think most of the generator instance resolution should probably be done inside the `register` method, since it's already needed there anyway

--- bin/cli.js
+++ bin/cli.js
@@ -2,70 +2,92 @@
 
 var path = require('path');
 var utils = require('../lib/utils');
-var Env = require('../lib/env');
+var errors = require('./errors');
+var generate = require('..');
 
-// parse argv (TODO: yargs!)
 var argv = require('minimist')(process.argv.slice(2), {
-  alias: {
-    help: 'h',
-    verbose: 'v'
-  }
+  alias: {help: 'h', verbose: 'v'}
 });
 
-/**
- * Run generate
- */
-
 function run(cb) {
-  // pre-process command line arguments
-  var args = utils.expandArgs(argv);
+  var cwd = process.cwd();
+  var root = cwd;
 
-  var baseEnv = createEnv(path.resolve(__dirname, '..'));
-  var Generate = require(baseEnv.module.path);
+  /**
+   * Set the working directory
+   */
 
-  var base = new Generate();
-  base.env = baseEnv;
-  base.fn = baseEnv.config.fn;
-  base.option(args);
-
-  var env = createEnv(process.cwd());
-  var config = env.config.fn;
-  var app = null;
-
-  // If true, we have an instance of `Generate`
-  if (utils.isObject(config) && config.isGenerate) {
-    app = config;
-    app.env = env;
-    app.option(args);
-    app.register('base', base, env);
-
-  // If true, we have a "generator" function
-  } else {
-    app = new Generate();
-    app.env = env;
-    app.option(args);
-    app.register('base', base, env);
-
-    // register local `generator.js` if it exists
-    if (typeof config === 'function') {
-      app.fn = config;
-      app.register(env.config.alias, config, env);
-    }
+  if (argv.cwd && cwd !== path.resolve(argv.cwd)) {
+    process.chdir(argv.cwd);
+    utils.timestamp('cwd changed to ' + utils.colors.yellow('~/' + argv.cwd));
   }
 
   /**
-   * Support `--emit` for debugging
-   * Examples:
-   *
-   *   # emit views as they're loaded
-   *   $ --emit view
-   *
-   *   # emit errors
-   *   $ --emit error
+   * Create the base "generate" instance
    */
 
-  if (args.emit && typeof args.emit === 'string') {
-    app.on(args.emit, console.log.bind(console));
+  var baseDir = path.resolve(__dirname, '..');
+  var baseEnv = createEnv('generator.js', baseDir);
+
+  // instantiate
+  var base = generate();
+  base.env = baseEnv;
+
+  // set the generate function on the instance
+  base.fn = require('../generator.js');
+
+  /**
+   * Get the generator.js to use
+   */
+
+  var generator = path.resolve(process.cwd(), 'generator.js');
+  if (!utils.exists(generator)) {
+    generator = path.resolve(__dirname, '../generator.js');
+  }
+
+  /**
+   * Create the user's "generate" instance
+   */
+
+  var fn = require(generator);
+  var env = createEnv('generator.js', process.cwd());
+  var app;
+
+  if (typeof fn === 'function') {
+    app = generate();
+    app.option(argv);
+    app.env = env;
+    app.fn = fn;
+    fn.call(app, app, app, env);
+
+  } else {
+    app = fn;
+    app.option(argv);
+    app.env = env;
+  }
+
+  /**
+   * Process command line arguments
+   */
+
+  var args = utils.processArgv(app, argv);
+  app.set('argv', args);
+
+  /**
+   * Show path to generator
+   */
+
+  utils.logConfigfile(root, generator);
+
+  /**
+   * Support `--emit` for debugging
+   *
+   * Example:
+   *   $ --emit data
+   */
+
+  if (argv.emit && typeof argv.emit === 'string') {
+    app.on(argv.emit, console.error.bind(console));
   }
 
   /**
@@ -77,7 +99,12 @@ function run(cb) {
     app.register(name, env.config.fn, env);
   });
 
+  /**
+   * Resolve generate generators
+   */
+
   app.env.resolve('generate-*/generator.js', {
+    configfile: 'generator.js',
     cwd: utils.gm
   });
 
@@ -85,28 +112,54 @@ function run(cb) {
 }
 
 /**
- * Run generators and their tasks
+ * Run
  */
 
 run(function(err, app) {
-  if (err) throw err;
+  if (err) handleError(err);
+
+  if (!app) {
+    process.exit(0);
+  }
+
+  /**
+   * Listen for errors
+   */
 
   app.on('error', function(err) {
-    console.error(err);
+    console.log(err);
   });
 
-  app.build(argv, function(err, cb) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
+  /**
+   * Run tasks
+   */
+
+  app.build(argv, function(err) {
+    if (err) throw err;
     utils.timestamp('finished');
     process.exit(0);
   });
 });
 
-function createEnv(cwd) {
-  var env = new Env('generator.js', 'generate', cwd);;
+/**
+ * Creat a new `env` object
+ */
+
+function createEnv(configfile, cwd) {
+  var env = new generate.Env(configfile, 'generate', cwd);;
   env.module.path = utils.tryResolve('generate');
   return env;
+}
+
+/**
+ * Handle CLI errors
+ */
+
+function handleError(err) {
+  if (typeof err === 'string' && errors[err]) {
+    console.error(errors[err]);
+  } else {
+    console.error(err);
+  }
+  process.exit(1);
 }
