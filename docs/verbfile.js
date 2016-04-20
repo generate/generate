10bef304cf3722cbbde6f312d98a43f0519d1130
jonschlinkert <jon.schlinkert@sellside.com>
2016-04-20 18:26:44 -0400

    convert verbfile to verb generator

--- docs/verbfile.js
+++ docs/verbfile.js
@@ -1,19 +1,14 @@
 'use strict';
 
-var verb = require('verb');
-var app = verb();
-
-/**
- * Support
- */
-
 var pipeline = require('./support/pipeline');
 var helpers = require('./support/helpers');
 
-app.helpers(helpers);
+module.exports = function(app, base) {
+  app.helpers(helpers);
 
-app.task('default', function(cb) {
-  return app.src('*.md')
-    .pipe(pipeline.md())
-    .pipe(app.dest('dist'));
-});
+  app.task('default', function(cb) {
+    return app.src('*.md')
+      .pipe(pipeline.md())
+      .pipe(app.dest('dist'));
+  });
+};
