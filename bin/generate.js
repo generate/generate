cd501704e284567ac3b2910024da307e0593e384
jonschlinkert <jon.schlinkert@sellside.com>
2016-05-09 15:27:21 -0400

    bump base-runner, update code in cli

--- bin/generate.js
+++ bin/generate.js
@@ -2,71 +2,37 @@
 
 process.env.GENERATE_CLI = true;
 
-var path = require('path');
-var argv = require('minimist')(process.argv.slice(2));
-var runner = require('base-runner');
+var generator =  require('../lib/generator');
 var commands = require('../lib/commands');
+var plugins =  require('../lib/plugins');
+var utils = require('../lib/utils');
+var argv = utils.yargs(process.argv.slice(2));
 var Generate = require('..');
 
-// lift-off options
-var options = {
-  name: 'generate',
-  runner: require('../package'),
-  configName: 'generator',
-  moduleParentId: path.resolve(__dirname, '..'),
-  lookup: lookup
-};
-
 /**
- * Initialize Generate CLI
+ * Initialize generate CLI
  */
 
-runner(Generate, options, argv, function(err, app, runnerContext) {
-  if (err) handleError(app, err);
+var config = {name: 'generate', configName: 'generator'};
 
-  /**
-   * Set `argv` on app.options
-   */
-
-  app.option(argv);
-
-  /**
-   * Load custom commands
-   */
-
-  commands(app, runnerContext);
-  app.register('defaults', require('../lib/generator.js'));
-
-  /**
-   * runnerContext
-   */
-
-  // get the config object from package.json
-  var config = app.pkg.get(runnerContext.env.name);
-  if (config && !args.noconfig) {
-    app.set('cache.config', config);
+plugins.runner(Generate, config, argv, function(err, app, ctx) {
+  if (err) {
+    console.log(err.stack);
+    process.exit(1);
   }
 
-  // set parsed and unparsed argv on `cache`
-  var args = app.argv(runnerContext.argv);
-  app.set('cache.argv', {
-    orig: argv,
-    parsed: runnerContext.argv,
-    processed: args
+  app.on('error', function(err) {
+    console.log(err);
+    console.log(err.stack);
+    process.exit(1);
   });
 
-  /**
-   * Custom lookup function for resolving generators
-   */
+  commands(app, ctx);
+  app.register('defaults', require('../lib/generator'));
+  app.option('lookup', lookup(app));
 
-  app.option('runner.new', 'files');
-
-  /**
-   * Process argv
-   */
-
-  app.cli.process(args, function(err) {
-    if (err) handleError(app, err);
+  app.cli.process(ctx.argv, function(err) {
+    if (err) app.emit('error', err);
     app.emit('done');
     process.exit();
   });
@@ -80,17 +46,8 @@ function lookup(app) {
   return function(key) {
     var patterns = [`generate-${key}`];
     if (/generate-/.test(key)) {
-      patterns.push(key);
+      patterns.unshift(key);
     }
     return patterns;
-  }
-}
-
-function handleError(app, err) {
-  if (app && app.handleError) {
-    app.handleError(err);
-  } else {
-    console.error(err.stack);
-    process.exit(1);
   }
 }
