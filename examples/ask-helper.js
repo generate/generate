c79e53eeb83c45c9bc2abace38be030d609c1044
jonschlinkert <jon.schlinkert@sellside.com>
2016-05-19 02:29:38 -0400

    adds examples

--- examples/ask-helper.js
+++ examples/ask-helper.js
@@ -1,26 +1,30 @@
 'use strict';
 
 var argv = require('minimist')(process.argv.slice(2));
+argv.cli = true;
 var generate = require('..');
 var app = generate(argv);
 
 app.engine('*', require('engine-base'));
 
 app.create('templates', {engine: '*'});
-app.template('doc', {
-  content: 'Name: <%= ask("beep") %>\nUsername: <%= ask("boop") %>\nName: <%= ask("beep") %>'
+app.template('one', {
+  content: 'Name: <%= ask("name") %>\nUsername: <%= ask("username") %>\nNext: <%= ask("name") %>'
 });
-app.template('doc2', {
-  content: 'Name: <%= ask("beep") %>\nUsername: <%= ask("boop") %>\nName: <%= ask("beep") %>'
+app.template('two', {
+  content: 'Name: <%= ask("name") %>\nUsername: <%= ask("username") %>\nNext: <%= ask("name") %>'
 });
 
-app.templates.getView('doc')
+app.templates.getView('one')
   .render(function(err, res) {
-    if (err) throw err;
+    if (err) return console.error(err);
+
     console.log(res.content);
-    app.templates.getView('doc2')
+    console.log();
+
+    app.templates.getView('two')
       .render(function(err, res) {
-        if (err) throw err;
+        if (err) return console.error(err);
         console.log(res.content);
       });
   });
