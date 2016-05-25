7857eedf5c05dc05cff2e25db31bf29674f5bf23
jonschlinkert <jon.schlinkert@sellside.com>
2016-05-25 18:53:39 -0400

    remove tests from assemble-core

---
+++ test/_suite.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var runner = require('base-test-runner')();
+var suite = require('base-test-suite');
+
+/**
+ * Run the tests in `base-test-suite`
+ */
+
+runner.on('templates', function(file) {
+  require(file.path)(require('..'));
+});
+
+runner.addFiles('templates', suite.test.templates);
+runner.addFiles('templates', suite.test['assemble-core']);
