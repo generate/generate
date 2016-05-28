1d8beb5d39a4034e8cc174016005c0279dbf4432
jonschlinkert <jon.schlinkert@sellside.com>
2016-05-28 15:24:36 -0400

    update scaffold example

--- examples/scaffold-basic/index.js
+++ examples/scaffold-basic/index.js
@@ -1,40 +1,105 @@
 'use strict';
 
+var path = require('path');
+var through = require('through2');
+var extname = require('gulp-extname');
+var scaffold = require('base-scaffold');
+var extend = require('extend-shallow');
+var parser = require('parser-front-matter');
 var Generate = require('../..');
-var generate = new Generate();
+var dest = path.resolve.bind(path, __dirname);
+var app = new Generate();
+app.use(scaffold());
+
+app.onLoad(/./, function(file, next) {
+  parser.parse(file, next);
+});
+
+app.on('error', function(err) {
+  console.log(err);
+});
 
 var Scaffold = require('scaffold');
-var scaffold = new Scaffold();
-
-/**
- * Add a basic "target" to our scaffold. Scaffolds are like
- * grunt "tasks" and can have any number of targets
- */
-
-scaffold.addTarget('abc', {
-  options: {
-    pipeline: generate.renderFile,
-    data: {
-      site: { title: 'My Blog' }
-    }
+var config = new Scaffold({
+  a: {
+    options: {
+      data: {title: 'Markdown'},
+      cwd: 'templates',
+      flatten: true,
+      destBase: dest('site/one'),
+      pipeline: ['render', 'extname', 'foo', 'bar']
+    },
+    files: [
+      {src: '*.hbs', dest: 'a', options: {pipeline: ['foo', 'bar', 'render', 'extname']}},
+      {src: '*.hbs', dest: 'b'},
+      {src: '*.hbs', dest: 'c'},
+      {src: '*.md', dest: 'd', data: {title: 'Foo'}},
+    ]
   },
-  src: 'templates/*.hbs',
-  dest: 'site',
+  b: {
+    options: {
+      data: {title: 'Baz'},
+      cwd: 'templates',
+      flatten: true,
+      destBase: dest('site/two'),
+      pipeline: app.renderFile
+    },
+    files: [
+      {src: '*.hbs', dest: 'a'},
+      {src: '*.hbs', dest: 'b'},
+      {src: '*.hbs', dest: 'c'},
+      {src: '*.md', dest: 'd', data: {title: 'Bar'}},
+    ]
+  }
 });
 
 /**
- * Template engine for rendering handlebars templates
+ * Site data
  */
 
-generate.engine('hbs', require('engine-handlebars'));
+app.data({
+  site: {
+    title: 'My Blog!'
+  }
+});
 
 /**
- * Generate the scaffold!
+ * Register pipeline plugins (can be gulp plugins)
  */
 
-generate.scaffold(scaffold)
-  .on('error', console.error)
-  .on('data', console.log)
-  .on('end', function() {
-    console.log('done!');
+app.plugin('extname', extname);
+app.plugin('render', app.renderFile);
+
+/**
+ * Custom pipeline plugins
+ */
+
+app.plugin('foo', function(options) {
+  return through.obj(function(file, enc, next) {
+    file.content += '\nfoo';
+    next(null, file);
   });
+});
+
+app.plugin('bar', function(options) {
+  return through.obj(function(file, enc, next) {
+    file.content += '\nbar';
+    next(null, file);
+  });
+});
+
+/**
+ * Register engines
+ */
+
+app.engine('hbs', require('engine-handlebars'));
+app.engine('md', require('engine-base'));
+
+/**
+ * Generate scaffold
+ */
+
+app.scaffold(config, function(err) {
+  if (err) throw err;
+  console.log('done!');
+});
---
+++ examples/scaffold-basic/templates/index.md
@@ -0,0 +1,6 @@
+---
+title: Home
+---
+Markdown
+Site title:<h1><%= site.title %></h1>
+Title:<h2><%= title %></h2>
