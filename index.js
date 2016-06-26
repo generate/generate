ac31511da84b6647b3893211fa9022c7b8fd51e1
jonschlinkert <jon.schlinkert@sellside.com>
2016-06-26 13:10:02 -0400

    numerous CLI updates

--- index.js
+++ index.js
@@ -7,7 +7,9 @@
 
 'use strict';
 
-var debug = require('debug')('base:generate');
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
 var Assemble = require('assemble-core');
 var plugins = require('./lib/plugins');
 var utils = require('./lib/utils');
@@ -44,50 +46,132 @@ Assemble.extend(Generate);
 
 Generate.prototype.initGenerate = function(opts) {
   Generate.emit('generate.preInit', this);
+  var self = this;
 
-  debug('initializing', __filename);
-  this.define('isApp', true);
-  this.option('help', {
-    command: 'gen',
-    configname: 'generator',
-    appname: 'generate'
-  });
+  // add `runner` to `app.cache.data`
+  this.data({runner: require('./package')});
 
-  // create `app.globals` store
-  this.define('globals', new utils.Store('globals', {
-    cwd: utils.resolveDir('~/')
-  }));
+  // custom lookup function for resolving generators
+  this.option('lookup', Generate.lookup);
 
   // custom `toAlias` function for resolving generators by alias
   this.option('toAlias', function(key) {
     return key.replace(/^generate-/, '');
   });
 
-  // add `runner` to `app.cache.data`
-  this.data({runner: require('./package')});
+  // format help menu
+  this.option('help', {
+    command: 'gen',
+    configname: 'generator',
+    appname: 'generate'
+  });
+
+  this.define('home', path.resolve.bind(path, os.homedir()));
+  this.define('destBase', {
+    configurable: true,
+    set: function(val) {
+      this._destBase = val;
+    },
+    get: function() {
+      return this._destBase || this.options.dest || this.cwd;
+    }
+  });
+
+  // create `macros` store
+  Object.defineProperty(this, 'macros', {
+    configurable: true,
+    get: function() {
+      return new utils.MacroStore({name: 'generate-macros'});
+    }
+  });
+
+  // create `app.common` store
+  Object.defineProperty(this, 'common', {
+    configurable: true,
+    get: function() {
+      return new utils.Store('common-config');
+    }
+  });
+
+  // create `app.globals` store
+  Object.defineProperty(this, 'globals', {
+    configurable: true,
+    get: function() {
+      return new utils.Store('generate-globals', {
+        cwd: utils.resolveDir('~/')
+      });
+    }
+  });
 
   // register async `ask` helper
   this.asyncHelper('ask', utils.ask(this));
 
   // load plugins
-  this.use(plugins.store());
+  this.use(plugins.store('generate'));
   this.use(plugins.generators());
   this.use(plugins.pipeline());
 
-  this.on('generator', function(alias, generator) {
-    if (generator && generator.env) {
-      generator.store = new utils.Store(generator.env.name, {
-        cwd: utils.resolveDir('~/.data-store')
+  /**
+   * Middleware
+   */
+
+  this.preWrite(/./, function(view, next) {
+    var askName = view.data.ask;
+    var hint = view.basename;
+    if (utils.isObject(askName)) {
+      var obj = askName;
+      hint = obj.default || hint;
+      askName = obj.rename;
+    }
+
+    function setValue(obj) {
+      var key = askName;
+      var val = obj[key];
+      if (val) view[key] = val;
+    }
+
+    if (typeof askName === 'string') {
+      var argv = self.get('cache.argv');
+      if (argv[askName]) {
+        setValue(argv);
+        next();
+        return;
+      }
+
+      self.question(askName, `What is the file.${askName}?`, {default: hint});
+      self.ask(askName, {save: false}, function(err, answers) {
+        if (err) return next(err);
+        if (answers[askName]) {
+          setValue(answers);
+        }
+        next();
       });
+    } else {
+      next();
     }
   });
 
-  this.option('lookup', function(key) {
-    var patterns = [`generate-${key}`];
-    if (/^generate-/.test(key) && !/^(verb|assemble|update)-/.test(key)) {
-      patterns.unshift(key);
+  this.preWrite(/./, utils.renameFile(self));
+  this.onLoad(/(^|[\\\/])templates[\\\/]/, function(view, next) {
+    var userDefined = self.home('templates', view.basename);
+    if (utils.exists(userDefined)) {
+      view.contents = fs.readFileSync(userDefined);
     }
-    return patterns;
+
+    if (/^templates[\\\/]/.test(view.relative)) {
+      view.path = path.join(self.cwd, view.basename);
+    }
+
+    utils.stripPrefixes(view);
+    utils.parser.parse(view, next);
+  });
+
+  /**
+   * Listeners
+   */
+
+  this.on('option', function(key, val) {
+    if (key === 'dest') self.cwd = val;
   });
 
   this.on('unresolved', function(search, app) {
@@ -97,9 +181,23 @@ Generate.prototype.initGenerate = function(opts) {
     }
   });
 
+  this.on('ask', function(answerVal, answerKey, question) {
+    if (typeof answerVal === 'undefined') {
+      var segs = answerKey.split('author.');
+      if (segs.length > 1) {
+        self.questions.answers[answerKey] = self.common.get(segs.pop());
+      }
+    }
+  });
+
+  /**
+   * CLI plugins
+   */
+
   if (utils.runnerEnabled(this)) {
-    this.initGenerateCli(opts);
+    this.initGenerateCLI(opts);
   }
+
   Generate.emit('generate.postInit', this);
 };
 
@@ -107,20 +205,8 @@ Generate.prototype.initGenerate = function(opts) {
  * Initialize CLI-specific plugins and view collections.
  */
 
-Generate.prototype.initGenerateCli = function(opts) {
-  // TODO: externalize most of these to plugin or generator
-  this.use(plugins.rename({replace: true}));
-  this.use(plugins.conflicts(opts));
-  this.use(plugins.runtimes(opts));
-  this.use(plugins.questions());
-  this.use(plugins.loader());
-  this.use(plugins.config());
-  this.use(plugins.cli());
-  this.use(plugins.npm());
-
-  // built-in view collections
-  this.create('templates');
-  this.create('files');
+Generate.prototype.initGenerateCLI = function(options) {
+  Generate.initGenerateCLI(this, options);
 };
 
 /**
@@ -131,19 +217,59 @@ Generate.prototype.initGenerateCli = function(opts) {
  */
 
 Generate.prototype.handleErr = function(err) {
+  return Generate.handleErr(this, err);
+};
+
+Generate.initGenerateCLI = function(app, options) {
+  plugins.runner.loadPlugins(app);
+  app.use(plugins.rename({replace: true}));
+  app.use(plugins.conflicts(options));
+  app.use(plugins.runtimes(options));
+  app.use(plugins.questions());
+  app.use(plugins.loader());
+  app.use(plugins.npm());
+
+  // built-in view collections
+  app.create('templates');
+};
+
+/**
+ * Temporary error handler method. until we implement better errors.
+ *
+ * @param {Object} `err` Object or instance of `Error`.
+ * @return {Object} Returns an error object, or emits `error` if a listener exists.
+ */
+
+Generate.handleErr = function(app, err) {
   if (!(err instanceof Error)) {
     err = new Error(err.toString());
   }
 
-  if (this.options.verbose) {
-    err = err.stack;
-  }
+  if (utils.isObject(app) && app.isApp) {
+    if (app.options.verbose) {
+      err = err.stack;
+    }
 
-  if (this.hasListeners('error')) {
-    this.emit('error', err);
+    if (app.hasListeners('error')) {
+      app.emit('error', err);
+    } else {
+      throw err;
+    }
   } else {
     throw err;
   }
+};
+
+/**
+ * Custom lookup function for resolving generators
+ */
+
+Generate.lookup = function(key) {
+  var patterns = [`generate-${key}`];
+  if (/^generate-/.test(key) && !/^(verb|assemble|update)-/.test(key)) {
+    patterns.unshift(key);
+  }
+  return patterns;
 };
 
 /**
@@ -156,12 +282,21 @@ Object.defineProperty(Generate.prototype, 'log', {
     function log() {
       return console.log.bind(console, utils.log.timestamp).apply(console, arguments);
     }
+
     log.warn = function(msg) {
+      return utils.logger('warning').apply(null, arguments);
+    };
+
+    log.warning = function(msg) {
       return utils.logger('warning', 'yellow').apply(null, arguments);
     };
 
     log.success = function() {
       return utils.logger('success', 'green').apply(null, arguments);
+    };
+
+    log.ok = function() {
+      return utils.logger('success').apply(null, arguments);
     };
 
     log.info = function() {
@@ -175,19 +310,6 @@ Object.defineProperty(Generate.prototype, 'log', {
     return log;
   }
 });
-
-/**
- * Expose static `is*` methods from Templates
- */
-
-Assemble._.plugin.is(Generate);
-
-/**
- * Expose static properties for unit tests
- */
-
-utils.define(Generate, 'utils', Assemble.utils);
-utils.define(Generate, '_', Assemble._);
 
 /**
  * Expose the `Generate` constructor
