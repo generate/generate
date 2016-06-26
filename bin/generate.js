44f17f4e9d2f360b31bb58c0bc52df42ee7a1b9a
jonschlinkert <jon.schlinkert@sellside.com>
2016-06-26 12:51:38 -0400

    update cli defaults

--- bin/generate.js
+++ bin/generate.js
@@ -1,21 +1,36 @@
 #!/usr/bin/env node
 
 process.env.GENERATE_CLI = true;
-
 require('set-blocking')(true);
 
-var generator =  require('../lib/generator');
+var util = require('util');
+var App = require('..');
 var commands = require('../lib/commands');
-var plugins =  require('../lib/plugins');
+var plugins = require('../lib/plugins');
+var tasks = require('../lib/tasks');
 var utils = require('../lib/utils');
-var argv = require('yargs-parser')(process.argv.slice(2));
-var Generate = require('..');
+var pkg = require('../package');
+var args = process.argv.slice(2);
+var argv = require('yargs-parser')(args, {
+  alias: {
+    verbose: 'v',
+    version: 'V'
+  }
+});
+
+if (argv.version) {
+  console.log('generate v' + pkg.version);
+  if (Object.keys(argv).length === 2 && !argv._.length) {
+    process.exit();
+  }
+}
 
 /**
  * Listen for errors on all instances
  */
 
-Generate.on('generate.preInit', function(app) {
+App.on('generate.preInit', function(app) {
+  app.set('cache.argv', argv);
   app.on('error', function(err) {
     console.log(err.stack);
     process.exit(1);
@@ -23,27 +38,84 @@ Generate.on('generate.preInit', function(app) {
 });
 
 /**
- * Initialize generate CLI
+ * Initialize CLI
  */
 
-var config = {name: 'generate', configName: 'generator'};
+App.on('generate.postInit', function(app) {
+  app.option(argv);
 
-plugins.runner(Generate, config, argv, function(err, app, ctx) {
+  if (app.macros.has(args)) {
+    app.macros.set(args);
+    var macro = {};
+    macro[args[0]] = args.slice(2).join(' ');
+    console.log('saved macro:', util.inspect(macro));
+    process.exit();
+  }
+
+  var idx = utils.firstIndex(args, ['-D', '--default']);
+  if (idx !== -1) {
+    args.splice(idx, 1);
+
+    if (args.indexOf('--del') !== -1) {
+      var tasks = app.store.get('defaultTasks');
+      app.store.del('defaultTasks');
+      app.log.warn('deleted default tasks:', tasks);
+      app.emit('done');
+      process.exit();
+    } else {
+      app.store.set('defaultTasks', args);
+      app.log.success('saved default tasks:', args);
+    }
+  }
+});
+
+/**
+ * Initialize Runner
+ */
+
+var options = {name: 'generate', configName: 'generator'};
+
+plugins.runner(App, options, argv, function(err, app, runnerContext) {
   if (err) handleErr(app, err);
 
-  app.set('cache.runnerContext', ctx);
-  app.register('defaults', require('../lib/generator'));
-  commands(app, ctx);
+  app.set('cache.runnerContext', runnerContext);
+  commands(app, runnerContext);
 
-  app.cli.process(ctx.argv, function(err) {
-    if (err) app.emit('error', err);
-    app.emit('done');
-    process.exit();
+  if (!app.generators.defaults) {
+    app.register('defaults', require('../lib/generator'));
+  }
+
+  var ctx = utils.extend({}, runnerContext);
+  var config = app.get('cache.config') || {};
+  ctx.argv.tasks = [];
+
+  app.config.process(config, function(err, config) {
+    if (err) return handleErr(app, err);
+
+    // reset `cache.config`
+    app.base.cache.config = config;
+
+    app.cli.process(ctx.argv, function(err) {
+      if (err) return handleErr(app, err);
+
+      var arr = tasks(app, ctx, argv);
+      app.log.success('running tasks:', arr);
+
+      if (ctx.env.configPath) {
+        app = app.generator('default', require(ctx.env.configPath));
+      }
+
+      app.generate(arr, function(err) {
+        if (err) return handleErr(app, err);
+        app.emit('done');
+        process.exit();
+      });
+    });
   });
 });
 
 /**
- * Custom lookup function for resolving generators
+ * Handle errors
  */
 
 function handleErr(app, err) {
