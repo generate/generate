f9dfa6572930b06267d1590a937e5f3ea69a6e83
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-07 11:49:25 -0400

    adds base-pkg

--- support/lib/common.js
+++ support/lib/common.js
@@ -1,8 +1,9 @@
 'use strict';
 
+var path = require('path');
 var helpers = require('./helpers');
 var isValid = require('is-valid-app');
-var path = require('path');
+var pkg = require('base-pkg');
 
 module.exports = function(options) {
   return function(app) {
@@ -11,7 +12,7 @@ module.exports = function(options) {
     app.use(require('generate-defaults'));
     app.use(require('verb-toc'));
     app.use(helpers());
-    app.use(helpers());
+    app.use(pkg());
 
     if (!app.docs) app.create('docs');
     app.option('renameKey', function(key, file) {
